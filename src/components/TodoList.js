f2cf889390fd7355a755a532350f53c2860c02b1
長谷川 裕大 <gssbtsm999@gmail.com>
2020-08-29 06:50:01 +0900

    Update

---
+++ src/components/TodoList.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './style.scss';
+
+function Todo({ todo, onCompletedChange }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={onCompletedChange}
+        data-id={todo.id}
+      />{' '}
+      <span className={todo.completed ? 'line-through' : undefined}>
+        {todo.text}
+      </span>
+    </li>
+  );
+}
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loaded: false, error: null, todos: [] };
+    this.url = 'https://5e6736691937020016fed762.mockapi.io/todos';
+    this.handleCompletedChange = this.handleCompletedChange.bind(this);
+  }
+
+  loadTodos(url) {
+    axios
+      .get(url)
+      .then(result => {
+        this.setState({ loaded: true, error: null, todos: result.data });
+      })
+      .catch(result => {
+        this.setState({ loaded: true, error: result, todos: [] });
+      });
+  }
+
+  componentDidMount() {
+    this.loadTodos(this.url);
+  }
+
+  handleCompletedChange(e) {
+    const checked = e.target.checked;
+    const changedId = e.target.dataset.id;
+    axios
+      .put(`${this.url}/${changedId}`, { completed: checked })
+      .then(result => {
+        const changedTodo = result.data;
+        console.log(changedTodo);
+        this.loadTodos(this.url);
+      });
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return <div>Loading...</div>;
+    } else
+      return (
+        <ul>
+          {this.state.todos.map(todo => (
+            <Todo
+              todo={todo}
+              key={todo.id}
+              onCompletedChange={this.handleCompletedChange}
+            />
+          ))}
+        </ul>
+      );
+  }
+}
+
+export default TodoList;
