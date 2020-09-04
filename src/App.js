0efce3c4afde171017dcbfd46a5006ac42b6836d
長谷川 裕大 <gssbtsm999@gmail.com>
2020-09-04 22:35:36 +0900

    コンポーネント構造を改正

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import classNames from 'classnames';
-import './tailwind.output.css';
-import Container from './components/Container';
 import Main from './components/Main';
-import HeadingLv1 from './components/HeadingLv1';
-import RoundedBox from './components/RoundedBox';
-import TodoView from './components/TodoView';
+import './tailwind.output.css';
 
 // TODO: モバイル前提として再設計する
 
 const App = () => {
-  return (
-    <Main>
-      <Container>
-        <section className={classNames('py-8')}>
-          <HeadingLv1>Todo list</HeadingLv1>
-          <RoundedBox>
-            <TodoView />
-          </RoundedBox>
-        </section>
-      </Container>
-    </Main>
-  );
+  return <Main />;
 };
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import classNames from 'classnames';
+import Container from './Container';
+import HeadingLv1 from './HeadingLv1';
+import RoundedBox from './RoundedBox';
+import TodoView from './TodoView';
 
-const Main = ({ children }) => {
-  return <main>{children}</main>;
+const Main = () => {
+  return (
+    <main>
+      <Container>
+        <section className={classNames('py-10')}>
+          <HeadingLv1>Todo list</HeadingLv1>
+          <RoundedBox>
+            <TodoView />
+          </RoundedBox>
+        </section>
+      </Container>
+    </main>
+  );
 };
 
 export default Main;
