f5943133b78d006e9caa68816f895af5750409c0
長谷川 裕大 <gssbtsm999@gmail.com>
2021-02-07 18:33:37 +0900

    U

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import classNames from 'classnames';
+import React from "react";
+import classnames from "classnames";
 
 const Container: React.FC = ({ children }) => {
-  return (
-    <div className={classNames('container', 'mx-auto', 'px-4')}>{children}</div>
-  );
+  return <div className={classnames("container", "mx-auto", "px-4")}>{children}</div>;
 };
 
 export default Container;
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import classNames from "classnames";
+import classnames from "classnames";
 
 const List: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div
       style={{ lineHeight: "32px" }}
-      className={classNames("py-2", "border-solid", "border-b", "border-gray-200")}
+      className={classnames("py-2", "border-solid", "border-b", "border-gray-200")}
     >
       {children}
     </div>
